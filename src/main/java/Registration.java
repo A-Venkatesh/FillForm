ea43b3db7d6fb0691d23292b1e40610f75afc969
A-Venkatesh <venkatesha@live.in>
2023-03-16 00:09:30 -0400

    added generic implementation strategy

--- src/main/java/Registration.java
+++ src/main/java/Registration.java
@@ -3,38 +3,42 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Registration {
     public static void main(String[] args) {
+        script();
+    }
 
+    static void script() {
         // Set the system property for the Chrome driver
-        String path = null;
-        try {
-            path = URLDecoder.decode(Registration.class.getClassLoader().getResource("chromedriver").getPath(), "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException(e);
-        }
+        String path;
+        path = URLDecoder.decode(Registration.class.getClassLoader().getResource("chromedriver").getPath(), StandardCharsets.UTF_8);
         System.out.println(path);
         System.setProperty("webdriver.chrome.driver", path);
         System.setProperty("webdriver.http.factory", "jdk-http-client");
         // Create a new instance of ChromeDriver
         WebDriver driver = new ChromeDriver();
-
         // Navigate to the registration page
         driver.get("https://www.amazon.com/rapids/register");
 
         // Find the input fields by their IDs and fill them out
-        WebElement firstName = driver.findElement(By.id("ap_customer_name"));
-        firstName.sendKeys("John");
+        List<WebElement> webElements = new SpreadSheet().getIds()
+                .stream()
+                .peek(System.out::println)
+                .map(id -> driver.findElement(By.id(id)))
+                .collect(Collectors.toList());
+        new SpreadSheet().getRows().stream().peek(System.out::println)
+                .forEach(row -> IntStream.range(0, webElements.size() - 1)
+                        .forEach(i -> {
+                            webElements.get(i).clear();
+                            webElements.get(i).sendKeys(row.get(i));
+                        }));
 
-        WebElement lastName = driver.findElement(By.id("ap_email"));
-        lastName.sendKeys("johndoe@example.com");
-        WebElement password = driver.findElement(By.id("ap_password"));
-        password.sendKeys("mysecretpassword");
-        WebElement email = driver.findElement(By.id("ap_password_check"));
-        email.sendKeys("mysecretpassword");
 
         // Submit the form
 //        WebElement submitButton = driver.findElement(By.id("continue"));
--- src/main/java/SpreadSheet.java
+++ src/main/java/SpreadSheet.java
@@ -1,2 +1,57 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.StreamSupport;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class SpreadSheet {
+    private Workbook workbook;
+
+    SpreadSheet(){
+        String fileName = null;
+        fileName = URLDecoder.decode(Registration.class.getClassLoader().getResource("InputSheet.xlsx").getPath(), StandardCharsets.UTF_8);
+        System.out.println(fileName);
+        try (FileInputStream file = new FileInputStream(new File(fileName));
+             Workbook workbook = new XSSFWorkbook(file)) {
+            this.workbook = workbook;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public List<String> getIds(){
+        String sheetName = "Sheet1";
+        Sheet sheet = workbook.getSheet(sheetName);
+
+        Row row = sheet.getRow(0);
+        List<String> headers = IntStream.range(0, row.getLastCellNum())
+                .mapToObj(i -> row.getCell(i).getStringCellValue())
+                .collect(Collectors.toList());
+        return headers;
+    }
+
+    public List<List<String>> getRows(){
+        String sheetName = "Sheet1";
+        List<List<String>> rows = StreamSupport.stream(workbook.getSheet(sheetName).spliterator(), false)
+                .skip(1) // skip the first row
+                .map(row -> StreamSupport.stream(row.spliterator(), false)
+                        .peek(cell -> System.out.println(cell.toString()))
+                        .map(Cell::getStringCellValue)
+                        .collect(Collectors.toList()))
+                .filter(row -> !row.isEmpty())
+                .collect(Collectors.toList());
+        return rows;
+    }
 }
